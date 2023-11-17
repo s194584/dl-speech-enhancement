f3ea284906f1ee9ddcbe5227c2efd6988dd939ab
Malthe Ørberg Pedersen <s194584@student.dtu.dk>
2023-11-17 15:06:24 +0100

    Fixed error and made pseudo batch

--- AudioDec/train_denoise.py
+++ AudioDec/train_denoise.py
@@ -1,14 +1,12 @@
 # import AudioDec
+import numpy as np
+
 from utils.audiodec import AudioDec, assign_model
 from dataloading.AudioDataset import AudioDataset
 from torch.utils.data import DataLoader, random_split
 import torch
 import math
-# import soundfile as sf
 import os
-# import numpy as np
-# from plot_audio_functions import plot_specgram
-# from trainer.denoise import Trainer as DenoiseTrainer
 
 from models.autoencoder.AudioDec import Generator as generator_audiodec
 import yaml
@@ -17,6 +15,10 @@ from losses import DiscriminatorAdversarialLoss
 from losses import FeatureMatchLoss
 from losses import GeneratorAdversarialLoss
 from losses import MultiMelSpectrogramLoss
+from clearml import Task
+
+task = Task.init('dl-speech-enhancement', 'fixed-error')
+
 
 ENVIRONMENT = 'LAPTOP'
 # ENVIRONMENT = 'HPC'
@@ -60,7 +62,7 @@ def define_splits(dataset, generator, train_percentage=0.7, val_percentage=0.15,
 def load_config(path_to_config):
     with open(path_to_config, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
-    
+
     return config
 
 
@@ -69,12 +71,12 @@ def define_optimizer_scheduler(model, config):
         torch.optim,
         config['generator_optimizer_type']
     )
-    
+
     # discriminator_optimizer_class = getattr(
     #     torch.optim,
     #     config['discriminator_optimizer_type']
     # )
-    
+
     optimizer = {
         'generator': generator_optimizer_class(
             model.parameters(),
@@ -90,12 +92,12 @@ def define_optimizer_scheduler(model, config):
         torch.optim.lr_scheduler,
         config.get('generator_scheduler_type', "StepLR"),
     )
-    
+
     # discriminator_scheduler_class = getattr(
     #     torch.optim.lr_scheduler,
     #     config.get('discriminator_scheduler_type', "StepLR"),
     # )
-    
+
     scheduler = {
         'generator': generator_scheduler_class(
             optimizer=optimizer['generator'],
@@ -106,31 +108,65 @@ def define_optimizer_scheduler(model, config):
         #     **config['discriminator_scheduler_params'],
         # ),
     }
-    
+
     return optimizer, scheduler
 
 
 def define_criterion(config, device):
     criterion = {
-    'gen_adv': GeneratorAdversarialLoss(
-        **config['generator_adv_loss_params']).to(device),
-    'dis_adv': DiscriminatorAdversarialLoss(
-        **config['discriminator_adv_loss_params']).to(device),
+        'gen_adv': GeneratorAdversarialLoss(
+            **config['generator_adv_loss_params']).to(device),
+        'dis_adv': DiscriminatorAdversarialLoss(
+            **config['discriminator_adv_loss_params']).to(device),
     }
     if config.get('use_feat_match_loss', False):
         criterion['feat_match'] = FeatureMatchLoss(
             **config.get('feat_match_loss_params', {}),
         ).to(device)
-        
+
     if config.get('use_mel_loss', False):
         criterion['mel'] = MultiMelSpectrogramLoss(
             **config['mel_loss_params'],
         ).to(device)
-    
+
     return criterion
 
 
-    
+# Constants for the length of noise
+number_of_intervals = 2
+length_of_interval = 3000  # Number of samples in the audio file ! SHOULD BE DIVISIBLE WITH HOP_SIZE in config file
+
+
+def mix_clean_noise(clean_sample, noise_sample):
+    y_samples = torch.tensor([])
+    x_samples = torch.tensor([])
+    for i in range(number_of_intervals):
+        rand_clean_start = torch.randint(0, len(clean_sample) - length_of_interval, (1,)).item()
+        rand_noise_start = torch.randint(0, len(noise_sample) - length_of_interval, (1,)).item()
+        clean_snippet = clean_sample[rand_clean_start:rand_clean_start + length_of_interval]
+        noise_snippet = noise_sample[rand_noise_start:rand_noise_start + length_of_interval]
+        mixed_snippet = add_noise(clean_snippet, noise_snippet, 10)  # TODO - add random SNR
+        if i == 0:
+            y_samples = clean_snippet
+            x_samples = mixed_snippet
+            continue
+        y_samples = torch.stack((y_samples, clean_snippet))
+        x_samples = torch.stack((x_samples, mixed_snippet))
+
+    return x_samples, y_samples
+
+
+def mix_clean_noise_batch(clean_batch, noise_batch):
+    y_batch = torch.tensor([])
+    x_batch = torch.tensor([])
+    for clean_sample in clean_batch:
+        for noise_sample in noise_batch:
+            x, y = mix_clean_noise(clean_sample, noise_sample)  # Maybe flatten
+            x_batch = torch.cat((x_batch, x))
+            y_batch = torch.cat((y_batch, y))
+    return x_batch, y_batch
+
+
 # Seeds for reproducibility #########
 generator_seed = 81
 generator = torch.Generator().manual_seed(generator_seed)
@@ -142,7 +178,7 @@ if -1 < 0:
 else:
     tx_device = 'cuda:cuda'
     rx_device = 'cuda:cuda'
-    
+
 device = torch.device(tx_device)
 
 # Loading model #####################
@@ -174,7 +210,7 @@ audiodec.load_receiver(encoder_checkpoint, decoder_checkpoint)
 # Loading data ######################
 sample_rate = 48000
 assert sample_rate == model_sample_rate, "The model's sample rate is not 48000Hz."
-batch_length = 1_000
+batch_length = 10_000
 snr = [20, 10, 3]
 clean_dataset = AudioDataset(CLEAN_PATH, batch_length, sample_rate)
 noise_dataset = AudioDataset(NOISE_PATH, batch_length, sample_rate)
@@ -182,8 +218,8 @@ noise_dataset = AudioDataset(NOISE_PATH, batch_length, sample_rate)
 clean_splits = define_splits(clean_dataset, generator)
 noise_splits = define_splits(noise_dataset, generator)
 
-train_clean_dataloader = DataLoader(clean_splits['train'], batch_size=1, shuffle=True, generator=generator)
-train_noise_dataloader = DataLoader(noise_splits['train'], batch_size=1, shuffle=True, generator=generator)
+train_clean_dataloader = DataLoader(clean_splits['train'], batch_size=2, shuffle=True, generator=generator)
+train_noise_dataloader = DataLoader(noise_splits['train'], batch_size=2, shuffle=True, generator=generator)
 # Loading Trainer ###################
 
 steps = 0
@@ -194,76 +230,68 @@ for i_batch, clean_sample_batch in enumerate(iter(train_clean_dataloader)):
     for j_batch, noise_sample_batch in enumerate(iter(train_noise_dataloader)):
         if j_batch == 2:
             break
-        
-        # Make pseudo batch
-        for clean_sample in clean_sample_batch:
-            for noise_sample in noise_sample_batch:
-                x_noisy = add_noise(clean_sample, noise_sample, snr=10)
-                x_target = clean_sample
-                
-                # Until we get a 
-                x_noisy = x_noisy[None, :].float()
-                x_target = x_target[None, :].float()
-                # print(x_noisy.dtype)
+
         # Perform training on pseudo batch
-                
-                # Send input through model
-                mode = 'train'
-                x_noisy = x_noisy.to(device)
-                x_target = x_target.to(device)
-                
-                # fix codebook
-                print("Fix codebook")
-                generator_model.quantizer.codebook.eval()
-                
-                # initialize generator loss
-                gen_loss = 0.0
+        # Goal: (B,C,1,T)
 
-                # main genertor operation
-                print("Main generator operation")
-                
-                ### TODO: channel first
-                y_nc, zq, z, vqloss, perplexity = generator_model(x_noisy)
-
-                # perplexity info
-                # self._perplexity(perplexity, mode=mode)
-
-                # vq loss
-                # gen_loss += self._vq_loss(vqloss, mode=mode)
-                print("VQ_loss")
-                vqloss = torch.sum(vqloss)
-                vqloss *= config["lambda_vq_loss"]
-                gen_loss += vqloss
-                
-                # metric loss
-                # gen_loss += self._metric_loss(y_nc, x_c, mode=mode)
-                
-                print("Mel_loss")
-                print(y_nc.shape)
-                print(x_target.shape)
-                mel_loss = criterion["mel"](y_nc, x_target)
-                mel_loss *= config["lambda_mel_loss"]
-                # self._record_loss('mel_loss', mel_loss, mode=mode)
-                gen_loss += mel_loss
-
-                # update generator
-                # self._record_loss('generator_loss', gen_loss, mode=mode)
-                # self._update_generator(gen_loss)
-                print("Optimizer")
-                optimizer["generator"].zero_grad()
-                gen_loss.backward()
-                
-                if config["generator_grad_norm"] > 0:
-                    torch.nn.utils.clip_grad_norm_(
-                        generator_model.parameters(),
-                        config["generator_grad_norm"],
-                    )
-                optimizer["generator"].step()
-                shceduler["generator"].step()
+        x_mixed, y_clean = mix_clean_noise_batch(clean_sample_batch, noise_sample_batch)
+        x_mixed = x_mixed.permute(0,2,1).float()  # (B,C,T)
+        y_clean = y_clean.permute(0,2,1).float()  # (B,C,T)
 
 
-                # update counts
-                print(steps)
-                steps += 1
-                # self.tqdm.update(1)
-                # self._check_train_finish()
+        # Send input through model
+        x_noisy = x_mixed.to(device)
+        x_target = y_clean.to(device)
+
+        # fix codebook
+        print("Fix codebook")
+        generator_model.quantizer.codebook.eval()
+
+        # initialize generator loss
+        gen_loss = 0.0
+
+        # main genertor operation
+        print("Main generator operation")
+
+        ### TODO: channel first
+        y_nc, zq, z, vqloss, perplexity = generator_model(x_noisy)
+
+        # perplexity info
+        # self._perplexity(perplexity, mode=mode)
+
+        # vq loss
+        # gen_loss += self._vq_loss(vqloss, mode=mode)
+        print("VQ_loss")
+        vqloss = torch.sum(vqloss)
+        vqloss *= config["lambda_vq_loss"]
+        gen_loss += vqloss
+
+        # metric loss
+        # gen_loss += self._metric_loss(y_nc, x_c, mode=mode)
+
+        print("Mel_loss")
+        print(y_nc.shape)
+        print(x_target.shape)
+        mel_loss = criterion["mel"](y_nc, x_target)
+        mel_loss *= config["lambda_mel_loss"]
+        # self._record_loss('mel_loss', mel_loss, mode=mode)
+        gen_loss += mel_loss
+
+        # update generator
+        # self._record_loss('generator_loss', gen_loss, mode=mode)
+        # self._update_generator(gen_loss)
+        print("Optimizer")
+        optimizer["generator"].zero_grad()
+        gen_loss.backward()
+
+        if config["generator_grad_norm"] > 0:
+            torch.nn.utils.clip_grad_norm_(
+                generator_model.parameters(),
+                config["generator_grad_norm"],
+            )
+        optimizer["generator"].step()
+        shceduler["generator"].step()
+
+        # update counts
+        print(steps)
+        steps += 1
