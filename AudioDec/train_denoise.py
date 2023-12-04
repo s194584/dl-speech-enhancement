54c9fa8d84e4abde7f79a655d4bab514f7297625
Malthe Ørberg Pedersen <s194584@student.dtu.dk>
2023-12-04 15:43:15 +0100

    Added discriminator loss

--- AudioDec/train_denoise.py
+++ AudioDec/train_denoise.py
@@ -7,6 +7,7 @@ from torch.optim import Adam
 from torchaudio import transforms
 
 from dataloader import CollaterAudio
+from losses import GeneratorAdversarialLoss, DiscriminatorAdversarialLoss
 from utils.audiodec import AudioDec, assign_model
 from dataloading.AudioDataset import AudioDataset
 from torch.utils.data import DataLoader, random_split
@@ -22,6 +23,7 @@ import random
 import os
 
 from models.autoencoder.AudioDec import Generator as generator_audiodec
+from models.vocoder.HiFiGAN import Discriminator as discriminator_hifigan
 import yaml
 from clearml import Task
 import soundfile as sf
@@ -40,11 +42,14 @@ if ENVIRONMENT == "LAPTOP":
     CLEAN_ROOT = "clean"
     NOISE_PATH = "corpus/train/noise"
     NOISE_ROOT = "noise"
-    task = Task.init("dl-speech-enhancement", "Laptop")
+
+    task_name = "Laptop-w-Dis"
+    task = Task.init("dl-speech-enhancement", task_name)
     logger = task.get_logger()
     torch.set_num_threads(4)
 elif ENVIRONMENT == "HPC":
-    task = Task.init("dl-speech-enhancement", "HPC-AudioDec-Fresh-Mel_L1-DefaultAdam")
+    task_name = "HPC-AudioDec-Fresh-Mel_L1-Adv"
+    task = Task.init("dl-speech-enhancement", task_name)
     logger = task.get_logger()
     CLEAN_PATH = "/work3/s164396/data/DNS-Challenge-4/datasets_fullband/clean_fullband/vctk_wav48_silence_trimmed"
     CLEAN_ROOT = "vctk_wav48_silence_trimmed"
@@ -114,11 +119,13 @@ else:
 device = torch.device(tx_device)
 
 # Loading model #####################
-model = "vctk_denoise"
+# model = "vctk_denoise"
+model = {}
 path_to_config = os.path.join("config", "denoise", "symAD_vctk_48000_hop300.yaml")
 
 config = load_config(path_to_config)
-generator_model = generator_audiodec(**config["generator_params"])
+model['generator'] = generator_audiodec(**config["generator_params"])
+model['discriminator'] = discriminator_hifigan(**config["discriminator_params"])
 
 # Optimizer
 # generator_optimizer_type: Adam
@@ -126,22 +133,29 @@ generator_model = generator_audiodec(**config["generator_params"])
 #     lr: 1.0e-4
 #     betas: [0.5, 0.9]
 #     weight_decay: 0.0
-optimizer = Adam(generator_model.parameters(),lr=5e-5)
+optimizer = {}
+optimizer['generator'] = Adam(model['generator'].parameters(),lr=5e-5)
+optimizer['discriminator'] = Adam(model['discriminator'].parameters(),lr=2.0e-4)
 
 
 # model_sample_rate, encoder_checkpoint, decoder_checkpoint = assign_model(model)
 # TODOO - Create own config file since we don't use theirs as much anymore
 def load_from_pretrained(checkpoint):
     state_dict = torch.load(checkpoint, map_location="cpu")
-    # generator_model.load_state_dict(state_dict["model"]["generator"])
-    generator_model.load_state_dict(state_dict)
+    # model['generator'].load_state_dict(state_dict["model"]["generator"])
+    model['generator'].load_state_dict(state_dict)
 
-encoder_checkpoint = os.path.join("exp","denoise","MelL1_Adam-adjusted_checkpoint-27915.pkl")
+## Generator
+encoder_checkpoint = os.path.join("exp","denoise","MelL1_Adam-adjusted_checkpoint-111661.pkl")
 load_from_pretrained(encoder_checkpoint)
-encoder = generator_model.encoder
+encoder = model['generator'].encoder
 encoder.to(device=tx_device)
-decoder = generator_model.decoder
+decoder = model['generator'].decoder
 decoder.to(device=tx_device)
+
+## Discriminator
+model['discriminator'] = model['discriminator'].to(device=tx_device)
+
 
 mae = nn.L1Loss().to(device)
 mse = nn.MSELoss().to(device)
@@ -174,6 +188,15 @@ measures = {
 }
 
 
+criterion = {
+        'gen_adv': GeneratorAdversarialLoss(
+            **config['generator_adv_loss_params']).to(tx_device),
+        'dis_adv': DiscriminatorAdversarialLoss(
+            **config['discriminator_adv_loss_params']).to(tx_device),
+        }
+
+
+
 def calculate_train_loss(pred, target):
     return 45.0*measures['Mel-L1'](pred, target)+30.0*measures['MAE'](pred, target)
 
@@ -189,7 +212,7 @@ def freeze_decoder(model):
         parameter.requires_grad = False
 
 
-# freeze_decoder(generator_model)
+# freeze_decoder(model['generator'])
 
 # Loading data ######################
 sample_rate = 48000
@@ -203,8 +226,8 @@ noise_splits = define_splits(noise_dataset, random_generator)
 collator = CollaterAudio(batch_length)
 
 if ENVIRONMENT == 'LAPTOP':
-    batch_size = 8
-    batch_size_noise = 8
+    batch_size = 5
+    batch_size_noise = 5
 else:
     batch_size = 8
     batch_size_noise = 8
@@ -252,22 +275,55 @@ def training_step(clean_sample_batch, mixed_sample_batch):
     encoded_batch = encoder(mixed_sample_batch)
     y_pred = decoder(encoded_batch)
 
-    loss = calculate_train_loss(y_pred, clean_sample_batch)
+    gen_loss = calculate_train_loss(y_pred, clean_sample_batch)
 
-    optimizer.zero_grad()
-    loss.backward()
+    gen_loss += criterion["gen_adv"](y_pred)
+
+    # feature matching loss
+    # if natural_p is not None:
+        # fm_loss = self.criterion["feat_match"](predict_p, natural_p)
+        # self._record_loss('feature_matching_loss', fm_loss, mode=mode)
+        # adv_loss += self.config["lambda_feat_match"] * fm_loss
+
+    # adv_loss *= self.config["lambda_adv"]
+
+
+    optimizer['generator'].zero_grad()
+    gen_loss.backward()
 
     if config["generator_grad_norm"] > 0:
         torch.nn.utils.clip_grad_norm_(
-            generator_model.parameters(),
+            model['generator'].parameters(),
             config["generator_grad_norm"],
         )
-    optimizer.step()
+    optimizer['generator'].step()
+    dis_loss = None
+    if model['discriminator'] != None:
+        with torch.no_grad():
+            encoded_batch = encoder(mixed_sample_batch)
+            y_pred = decoder(encoded_batch)
+
+        p = model["discriminator"](clean_sample_batch)
+        p_ = model["discriminator"](y_pred.detach())
+
+        # discriminator loss & update discriminator
+        real_loss, fake_loss = criterion["dis_adv"](p_, p)
+        dis_loss = real_loss + fake_loss
+
+        optimizer["discriminator"].zero_grad()
+        dis_loss.backward()
+        if config["discriminator_grad_norm"] > 0:
+            torch.nn.utils.clip_grad_norm_(
+                model["discriminator"].parameters(),
+                config["discriminator_grad_norm"],
+            )
+        optimizer["discriminator"].step()
+
 
     if record_audio_snippets:
         save_snippets('prediction', y_pred.detach())
 
-    return loss
+    return gen_loss, dis_loss
 
 
 def validation_step(clean_sample_batch, mixed_sample_batch):
@@ -283,10 +339,9 @@ def validation_step(clean_sample_batch, mixed_sample_batch):
 
 
 start_time = time.perf_counter()
-steps = 0
+steps = 111661
 epochs = [i for i in range(500)]
 # Maybe have an epoch here
-steps = 27916
 train_acc_batch_size = 0
 train_steps = 0
 record_audio_snippets = False
@@ -324,10 +379,13 @@ def save_snippets(type, sample_batch):
 for epoch in epochs:
     # Training loop #####################
     i_batch = 0
-    train_losses = []
+    train_losses = {'generator':[],'discriminator':[]}
     print("New epoch")
     for i_batch, (clean_sample_batch, noise_sample_batch) in enumerate(
             zip(train_clean_dataloader, train_noise_dataloader)):
+
+        if ENVIRONMENT == "LAPTOP" and i_batch == 3:
+            break
         if len(clean_sample_batch) > len(noise_sample_batch):
             clean_sample_batch = clean_sample_batch[: len(noise_sample_batch)]
         else:
@@ -339,25 +397,28 @@ for epoch in epochs:
         if record_audio_snippets:
             save_snippets('target', mixed_samples)
 
-        if ENVIRONMENT == "LAPTOP" and i_batch == 3:
-            break
-        loss = training_step(clean_sample_batch, mixed_samples).item()
-        train_losses.append(loss)
+        gen_loss, dis_loss = training_step(clean_sample_batch, mixed_samples)
+        gen_loss, dis_loss = gen_loss.item(), dis_loss.item()
+        train_losses['generator'].append(gen_loss)
+        train_losses['discriminator'].append(dis_loss)
         steps += 1
         train_steps += 1
 
         if steps % 100 == 0 or ENVIRONMENT == "LAPTOP":
             report_time('Training')
             logger.report_scalar(
-                "Batch-loss", "Train", loss, iteration=train_steps
+                "Gen-batch-loss", "Train", gen_loss, iteration=train_steps
+            )
+            logger.report_scalar(
+                "Dis-batch-loss", "Train", dis_loss, iteration=train_steps
             )
 
-    avg_training_loss = np.mean(train_losses)
+    avg_training_loss = np.mean(train_losses['generator'])
     # Do a checkpoint
     check_point_path = os.path.join(
-        "exp", "denoise", "fresh", "si-sdr", f"checkpoint-{train_steps}.pkl"
+        "exp", "denoise", "fresh", f"{task_name}checkpoint-{train_steps}.pkl"
     )
-    torch.save(generator_model.state_dict(), check_point_path)
+    torch.save(model['generator'].state_dict(), check_point_path)
 
     # Validation loop ###################
     val_loss = 0
