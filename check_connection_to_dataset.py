2812707d09318278613c3318870b7fb87b852cd9
Malthe Ørberg Pedersen <s194584@student.dtu.dk>
2023-12-21 19:59:36 +0100

    Deleted stuff in root folder

--- check_connection_to_dataset.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-FOLDER_PATH = "/work3/s164396/data/DNS-Challenge-4/"
-FILE_NAME = "dns4-datasets-files-sha1.csv"
-
-
-with open(FOLDER_PATH+FILE_NAME,'r') as file:
-  csvfile = pd.read_csv(file)
-  print(csvfile)
--- testing-torchaudio.py
+++
@@ -1,9 +0,0 @@
-import os
-import torch
-import argparse
-import numpy as np
-import soundfile as sf
-
-
-
-data, fs = sf.read(args.input, always_2d=True)
